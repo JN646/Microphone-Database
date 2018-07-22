50a3b630fbf13384f58f3527b0ac29ff8b092a57
JN646 <josh.ginn@hotmail.co.uk>
2018-07-22 11:39:56 +0100

    Added Links to functions. Type Column added.

--- index.php
+++ index.php
@@ -8,24 +8,33 @@
   <body>
     <h1>Microphone Database</h1>
     <p>List of microphones and specs. A simple programming project.</p>
+    <p><a href="#">Add</a></p>
     <?php
     // ACTIVE RESULTS
-    $activesql = "SELECT * FROM crud ORDER BY model ASC";
+    $activesql = "SELECT * FROM crud ORDER BY make ASC";
     if ($result = mysqli_query($mysqli, $activesql)) {
         if (mysqli_num_rows($result) > 0) {
             ?>
     <table>
       <tr>
+        <th>ID</th>
         <th>Make</th>
         <th>Model</th>
+        <th>Type</th>
+        <th>Edit</th>
+        <th>Delete</th>
       </tr>
 
       <?php
           while ($row = mysqli_fetch_array($result)) {
               // Draw Table.
               echo "<tr>";
+              echo "<td>" . $row['id'] . "</td>";
               echo "<td>" . $row['make'] . "</td>";
               echo "<td>" . $row['model'] . "</td>";
+              echo "<td>" . $row['type'] . "</td>";
+              echo "<td><a href='#'>Edit</a></td>";
+              echo "<td><a href='#'>Delete</a></td>";
               echo "</tr>";
           }
             echo "</table>";
