562565e72d0c649b45cb5dc3454a33276b3c1d3e
Josh <josh.ginn@hotmail.co.uk>
2018-11-25 19:59:27 +0000

    Cleaned up View page.

--- view.php
+++ view.php
@@ -17,22 +17,47 @@
     $discontinued = $microphone['discontinued'];
     $polarpattern = $microphone['polarpattern'];
     $notes = $microphone['notes'];
+
+    if ($price == 0) {
+      $price = 'N/A';
+    }
+
+    if ($discontinued == 0) {
+      $discontinued = 'Still Available';
+    }
+
+    if ($discontinued == 1) {
+      $discontinued = 'No Longer Available';
+    }
    ?>
 
    <!-- Header -->
-    <h1><?php echo $make . " " . $model ?></h1>
+    <div class='col-md-12'>
+      <h1><?php echo $make . " " . $model ?></h1>
 
-    <!-- Paras -->
-    <p><strong>Type:</strong> <?php echo $type ?></p>
-    <p><strong>Price:</strong> <?php echo $price ?></p>
-    <p><strong>Discontinued:</strong> <?php echo $discontinued ?></p>
-    <p><strong>Polar Pattern:</strong> <?php echo $polarpattern ?></p>
-    <!-- Polar Pattern Image -->
-    <img src="img/<?php echo polarPatternImage($polarpattern); ?>" width=100px alt="">
-    <h3>Notes:</h3>
-    <p><?php echo $notes ?></p>
+      <!-- Paras -->
+      <p><strong>Type:</strong> <?php echo $type ?></p>
+      <p><strong>Price:</strong> <?php echo $price ?></p>
+      <p><strong>Discontinued:</strong> <?php echo $discontinued ?></p>
+      <p><strong>Polar Pattern:</strong> <?php echo $polarpattern ?></p>
+      <!-- Polar Pattern Image -->
+      <img src="img/<?php echo polarPatternImage($polarpattern); ?>" width=100px alt="">
+
+      <br>
+
+      <h3>Notes:</h3>
+      <div class="border border-primary">
+        <?php
+        if ($notes == '') {
+          echo "<p class='text-center'>There is currently no description.</p>";
+        } else {
+          echo "<p>" . $notes . "</p>";
+        }
+        ?>
+      </div>
+    </div>
     <br>
 
     <!-- Back Button -->
-    <p><a href="index.php">Back</a></p></div>
+    <p><a href="index.php"><i class="fas fa-arrow-circle-left fa-2x"></i></a></p></div>
     <?php include 'partials/_footer.php'; ?>
