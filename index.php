f358a8ed49f726811ab5a5d9801cd85896731e03
Josh <josh.ginn@hotmail.co.uk>
2018-11-25 17:57:17 +0000

    Design Changes.

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
+<!-- Header Partial -->
 <?php include_once 'partials/_header.php' ?>
-  <div class='container'>
+
+<!-- Container -->
+  <div id='bodyContainer' class='container'>
+
+    <!-- Notification Block -->
     <?php if (isset($_SESSION['message'])): ?>
         <div class="msg">
           <?php
@@ -8,6 +13,8 @@
           ?>
         </div>
       <?php endif ?>
+
+      <!-- Header -->
       <h1>Microphone Database <span class='badge'>(<?php echo countMicrophones($mysqli); ?>)</span></h1>
       <p>A database of common microphones, specs and information. A simple programming project.</p>
 
@@ -34,96 +41,97 @@
       }
       ?>
 
-  <div class='border'>
-    <div class='col-md-12'>
-    <!-- Control Form -->
-      <?php if ($update == true): ?>
-        <h2>Update</h2>
-      <?php else: ?>
-        <h2>Add</h2>
-      <?php endif ?>
-      <form class='' method="post" action="server.php" >
-      	<input type="hidden" name="id" value="<?php echo $id; ?>">
+      <!-- Form -->
+      <div class='border border-primary'>
+        <div class='col-md-12'>
+        <!-- Control Form -->
+          <?php if ($update == true): ?>
+            <h2>Update</h2>
+          <?php else: ?>
+            <h2>Add</h2>
+          <?php endif ?>
+          <form class='' method="post" action="server.php" >
+          	<input type="hidden" name="id" value="<?php echo $id; ?>">
+
+          <div class='form-row'>
+            <!-- Make -->
+          <div class='col'>
+              <div class="form-group">
+              		<label class="">Make</label><br>
+              		<input class='form-control' type="text" name="make" value="<?php echo $make; ?>">
+              	</div>
+              </div>
+
+              <div class='col'>
+                <!-- Model -->
+              	<div class="form-group">
+              		<label class="">Model</label><br>
+              		<input class='form-control' type="text" name="model" value="<?php echo $model; ?>">
+              	</div>
+              </div>
+          </div>
 
       <div class='form-row'>
-        <!-- Make -->
-      <div class='col'>
-          <div class="form-group">
-          		<label class="">Make</label><br>
-          		<input class='form-control' type="text" name="make" value="<?php echo $make; ?>">
+        <!-- Type -->
+          <div class='col'>
+            <div class="form-group">
+          		<label class="">Type</label><br>
+              <select class="form-control" name="type">
+                <option value="">Please Select</option>
+                <option value="Dynamic">Dynamic</option>
+                <option value="Condenser">Condenser</option>
+                <option value="Ribbon">Ribbon</option>
+              </select>
           	</div>
           </div>
 
           <div class='col'>
-            <!-- Model -->
+            <!-- Polar Patterns -->
+            <div class="form-group">
+              <label class="">Polar Pattern</label><br>
+              <select class="form-control" name="polarpattern">
+                <option value="">Please Select</option>
+                <option value="Omnidirectional">Omnidirectional</option>
+                <option value="Cardioid">Cardioid</option>
+                <option value="Super-Cardioid">Super-Cardioid</option>
+              </select>
+            </div>
+          </div>
+
+          <!-- Price -->
+            <div class='col'>
+              <div class="form-group">
+            		<label class="">Price</label><br>
+                <input class='form-control' type="text" name="price" value="<?php echo $price; ?>">
+            	</div>
+            </div>
+
+            <div class='col'>
+              <!-- Discontinued -->
+              <div class="form-group">
+                <label class="">Discontinued</label><br>
+                <input class='form-control' type="text" name="discontinued" value="<?php echo $discontinued; ?>">
+              </div>
+            </div>
+          </div>
+
+            <!-- Notes -->
           	<div class="form-group">
-          		<label class="">Model</label><br>
-          		<input class='form-control' type="text" name="model" value="<?php echo $model; ?>">
+          		<label class="">Notes</label><br>
+          		<textarea class="form-control" name="notes"><?php echo $notes; ?></textarea>
           	</div>
-          </div>
-      </div>
 
-  <div class='form-row'>
-    <!-- Type -->
-      <div class='col'>
-        <div class="form-group">
-      		<label class="">Type</label><br>
-          <select class="form-control" name="type">
-            <option value="">Please Select</option>
-            <option value="Dynamic">Dynamic</option>
-            <option value="Condenser">Condenser</option>
-            <option value="Ribbon">Ribbon</option>
-          </select>
-      	</div>
-      </div>
-
-      <div class='col'>
-        <!-- Polar Patterns -->
-        <div class="form-group">
-          <label class="">Polar Pattern</label><br>
-          <select class="form-control" name="polarpattern">
-            <option value="">Please Select</option>
-            <option value="Omnidirectional">Omnidirectional</option>
-            <option value="Cardioid">Cardioid</option>
-            <option value="Super-Cardioid">Super-Cardioid</option>
-          </select>
+            <!-- Submit Buttons -->
+          	<div class="form-group">
+          		<?php if ($update == true): ?>
+          			<button class="btn btn-primary" type="submit" name="update">Update</button>
+          		<?php else: ?>
+          			<button class="btn btn-success" type="submit" name="save">Add</button>
+          		<?php endif ?>
+          	</div>
+          </form>
         </div>
       </div>
-
-      <!-- Price -->
-        <div class='col'>
-          <div class="form-group">
-        		<label class="">Price</label><br>
-            <input class='form-control' type="text" name="price" value="<?php echo $price; ?>">
-        	</div>
-        </div>
-
-        <div class='col'>
-          <!-- Discontinued -->
-          <div class="form-group">
-            <label class="">Discontinued</label><br>
-            <input class='form-control' type="text" name="discontinued" value="<?php echo $discontinued; ?>">
-          </div>
-        </div>
-      </div>
-
-        <!-- Notes -->
-      	<div class="form-group">
-      		<label class="">Notes</label><br>
-      		<textarea class="form-control" name="notes"><?php echo $notes; ?></textarea>
-      	</div>
-
-        <!-- Submit Buttons -->
-      	<div class="form-group">
-      		<?php if ($update == true): ?>
-      			<button class="btn" type="submit" name="update">Update</button>
-      		<?php else: ?>
-      			<button class="btn" type="submit" name="save">Add</button>
-      		<?php endif ?>
-      	</div>
-      </form>
-    </div>
-  </div>
 
       <br>
 
@@ -133,27 +141,30 @@
       if ($result = mysqli_query($mysqli, $activesql)) {
           if (mysqli_num_rows($result) > 0) {
               ?>
-      <table class='table'>
-        <tr>
-          <th class='text-center'>Make</th>
-          <th class='text-center'>Model</th>
-          <th class='text-center'>Type</th>
-          <th class='text-center'>Polar Pattern</th>
-  			  <th class='text-center' colspan="3">Action</th>
-        </tr>
-
+      <table id='resultTable' class='table table-bordered'>
+        <thead class="thead-dark">
+          <tr>
+            <th class='text-center'>Make</th>
+            <th class='text-center'>Model</th>
+            <th class='text-center'>Type</th>
+            <th class='text-center'>Polar Pattern</th>
+            <th class='text-center' colspan="3">Action</th>
+          </tr>
+        </thead>
         <?php
             while ($row = mysqli_fetch_array($result)) {
                 // Draw Table.
-                echo "<tr>";
-                echo "<td>" . $row['make'] . "</td>";
-                echo "<td>" . $row['model'] . "</td>";
-                echo "<td>" . $row['type'] . "</td>";
-                echo "<td>" . $row['polarpattern'] . "</td>";
-                echo "<td><a href='view.php?id=" . $row['id'] . "' class='view_btn'><i class='fas fa-eye'></i></a></td>";
-                echo "<td><a href='index.php?edit=" . $row['id'] . "' class='edit_btn'><i class='fas fa-edit'></i></a></td>";
-                echo "<td><a href='server.php?del=" . $row['id'] . "' class='del_btn'><i class='far fa-trash-alt'></i></a></td>";
-                echo "</tr>";
+                echo "<tbody>";
+                  echo "<tr>";
+                    echo "<td>" . $row['make'] . "</td>";
+                    echo "<td>" . $row['model'] . "</td>";
+                    echo "<td>" . $row['type'] . "</td>";
+                    echo "<td>" . $row['polarpattern'] . "</td>";
+                    echo "<td class='text-center'><a href='view.php?id=" . $row['id'] . "' class='view_btn'><i class='fas fa-eye'></i></a></td>";
+                    echo "<td class='text-center'><a href='index.php?edit=" . $row['id'] . "' class='edit_btn'><i class='fas fa-edit'></i></a></td>";
+                    echo "<td class='text-center'><a href='server.php?del=" . $row['id'] . "' class='del_btn'><i class='far fa-trash-alt'></i></a></td>";
+                  echo "</tr>";
+                echo "</tbody>";
             }
               echo "</table>";
 
--- view.php
+++ view.php
@@ -1,12 +1,15 @@
 <?php include_once 'partials/_header.php' ?>
-<div class='container'>
+<div id='bodyContainer' class='container'>
   <?php
+    // Variables
     $id = $_GET["id"];
 
+    // Get SQL
     $activesql = "SELECT * FROM crud WHERE id = $id";
     $result = mysqli_query($mysqli, $activesql);
     $microphone = mysqli_fetch_array($result);
 
+    // Map fields
     $make = $microphone['make'];
     $model = $microphone['model'];
     $type = $microphone['type'];
@@ -16,15 +19,21 @@
     $notes = $microphone['notes'];
    ?>
 
+   <!-- Header -->
     <h1><?php echo $make . " " . $model ?></h1>
+
+    <!-- Paras -->
     <p><strong>Type:</strong> <?php echo $type ?></p>
     <p><strong>Price:</strong> <?php echo $price ?></p>
     <p><strong>Discontinued:</strong> <?php echo $discontinued ?></p>
     <p><strong>Polar Pattern:</strong> <?php echo $polarpattern ?></p>
+    <!-- Polar Pattern Image -->
     <img src="img/<?php echo polarPatternImage($polarpattern); ?>" width=100px alt="">
     <h3>Notes:</h3>
     <p><?php echo $notes ?></p>
     <br>
+
+    <!-- Back Button -->
     <p><a href="index.php">Back</a></p></div>
   </body>
 </html>
